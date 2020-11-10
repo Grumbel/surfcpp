5ce10864464b1a5ef0852b64f0ca601c1a403841
Ingo Ruhnke <grumbel@gmail.com>
2020-11-10 19:33:02 +0100

    Add pixel accessor functions that convert to float

--- test/pixel_test.cpp
+++ test/pixel_test.cpp
@@ -21,4 +21,45 @@ TEST(PixelTest, equality)
   EXPECT_NE((surf::GreyscalePixel{1}), (surf::GreyscalePixel{4}));
 }
 
+TEST(PixelTest, max)
+{
+  EXPECT_EQ(surf::RGB8Pixel::max(), 255);
+  EXPECT_EQ(surf::RGB16Pixel::max(), 65535);
+  EXPECT_EQ(surf::RGB32Pixel::max(), 4294967295);
+
+  EXPECT_EQ(surf::RGB32fPixel::max(), 1.0f);
+  EXPECT_EQ(surf::RGB64fPixel::max(), 1.0);
+}
+
+TEST(PixelTest, alpha)
+{
+  EXPECT_EQ(surf::alpha(surf::RGB8Pixel{}), 255);
+  EXPECT_EQ(surf::alpha(surf::RGB16Pixel{}), 65535);
+  EXPECT_EQ(surf::alpha(surf::RGB32Pixel{}), 4294967295);
+}
+
+TEST(PixelTest, alpha_f)
+{
+  EXPECT_EQ(surf::alpha_f(surf::RGB8Pixel{}), 1.0f);
+  EXPECT_EQ(surf::alpha_f(surf::RGB16Pixel{}), 1.0f);
+  EXPECT_EQ(surf::alpha_f(surf::RGB32Pixel{}), 1.0f);
+}
+
+TEST(PixelTest, f2value)
+{
+  EXPECT_EQ(surf::f2value<surf::RGB8Pixel>(1.0f), 255);
+  EXPECT_EQ(surf::f2value<surf::RGB16Pixel>(1.0f), 65535);
+  EXPECT_EQ(surf::f2value<surf::RGB32Pixel>(1.0f), 4294967295);
+
+  EXPECT_EQ(surf::f2value<surf::RGB8Pixel>(0.0f), 0);
+  EXPECT_EQ(surf::f2value<surf::RGB16Pixel>(0.0f), 0);
+  EXPECT_EQ(surf::f2value<surf::RGB32Pixel>(0.0f), 0);
+
+  EXPECT_EQ(surf::f2value<surf::RGB32fPixel>(1.0f), 1.0f);
+  EXPECT_EQ(surf::f2value<surf::RGB64fPixel>(1.0f), 1.0);
+
+  EXPECT_EQ(surf::f2value<surf::RGB32fPixel>(0.0f), 0.0f);
+  EXPECT_EQ(surf::f2value<surf::RGB64fPixel>(0.0f), 0.0);
+}
+
 /* EOF */
