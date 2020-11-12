6c51a9ba97e2c33fb0932cd8ae921dc4976e9a26
Ingo Ruhnke <grumbel@gmail.com>
2020-11-12 21:03:36 +0100

    Add more benchmarks

--- benchmark/benchmark_blit.cpp
+++ benchmark/benchmark_blit.cpp
@@ -42,6 +42,48 @@ void BM_blit__copy(benchmark::State& state)
   }
 }
 
+void BM_blit__blend(benchmark::State& state)
+{
+  PixelData<RGBPixel> src(SRCSIZE, RGBPixel{255, 255, 255});
+  PixelData<RGBPixel> dst(DSTSIZE, RGBPixel{0, 0, 0});
+
+  while (state.KeepRunning()) {
+    for (int y = 0; y < 1024; y += 100) {
+      for (int x = 0; x < 1024; x += 100) {
+        blend(pixel_copy<RGBPixel, RGBPixel>(), src, geom::irect(src.get_size()), dst, geom::ipoint(x, y));
+      }
+    }
+  }
+}
+
+void BM_blit__blend_blend(benchmark::State& state)
+{
+  PixelData<RGBPixel> src(SRCSIZE, RGBPixel{255, 255, 255});
+  PixelData<RGBPixel> dst(DSTSIZE, RGBPixel{0, 0, 0});
+
+  while (state.KeepRunning()) {
+    for (int y = 0; y < 1024; y += 100) {
+      for (int x = 0; x < 1024; x += 100) {
+        blend(pixel_blend<RGBPixel, RGBPixel>(), src, geom::irect(src.get_size()), dst, geom::ipoint(x, y));
+      }
+    }
+  }
+}
+
+void BM_blit__blend_add(benchmark::State& state)
+{
+  PixelData<RGBPixel> src(SRCSIZE, RGBPixel{255, 255, 255});
+  PixelData<RGBPixel> dst(DSTSIZE, RGBPixel{0, 0, 0});
+
+  while (state.KeepRunning()) {
+    for (int y = 0; y < 1024; y += 100) {
+      for (int x = 0; x < 1024; x += 100) {
+        blend(pixel_add<RGBPixel, RGBPixel>(), src, geom::irect(src.get_size()), dst, geom::ipoint(x, y));
+      }
+    }
+  }
+}
+
 void BM_blit__slow(benchmark::State& state)
 {
   PixelData<RGBPixel> src(SRCSIZE, RGBPixel{255, 255, 255});
@@ -84,13 +126,31 @@ void BM_blit__slow_convert(benchmark::State& state)
   }
 }
 
+void BM_blit__slow_convert_self(benchmark::State& state)
+{
+  PixelData<RGBPixel> src(SRCSIZE, RGBPixel{255, 255, 255});
+  PixelData<RGBPixel> dst(DSTSIZE, RGBPixel{0, 0, 0});
+
+  while (state.KeepRunning()) {
+    for (int y = 0; y < 1024; y += 100) {
+      for (int x = 0; x < 1024; x += 100) {
+        blit__slow(src, dst, geom::ipoint(x, y));
+      }
+    }
+  }
+}
+
 } // namespace
 
 BENCHMARK(BM_blit);
+BENCHMARK(BM_blit__blend);
+BENCHMARK(BM_blit__blend_add);
+BENCHMARK(BM_blit__blend_blend);
 BENCHMARK(BM_blit__copy);
 BENCHMARK(BM_blit__slow);
 
 BENCHMARK(BM_blit__convert);
 BENCHMARK(BM_blit__slow_convert);
+BENCHMARK(BM_blit__slow_convert_self);
 
 /* EOF */
