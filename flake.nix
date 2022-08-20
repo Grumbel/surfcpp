b5e560e866682000996279c84051a2d314741af0
Ingo Ruhnke <grumbel@gmail.com>
2022-08-20 14:17:14 +0200

    Replace manual SDL2 build with SDL2-win32 flake

--- flake.nix
+++ flake.nix
@@ -15,34 +15,20 @@
     geomcpp.inputs.nixpkgs.follows = "nixpkgs";
     geomcpp.inputs.tinycmmc.follows = "tinycmmc";
 
-    SDL2_src.url = "https://libsdl.org/release/SDL2-devel-2.0.22-mingw.tar.gz";
-    SDL2_src.flake = false;
+    SDL2-win32.url = "github:grumnix/SDL2-win32";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc, geomcpp, logmich, SDL2_src }:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc, geomcpp, logmich, SDL2-win32 }:
     tinycmmc.lib.eachSystemWithPkgs (pkgs:
       {
         packages = rec {
           default = surfcpp;
 
-          SDL2 = if pkgs.targetPlatform.isWindows then
-            pkgs.stdenv.mkDerivation {
-            pname = "SDL2";
-            version = "2.0.22";
-            src = SDL2_src;
-            installPhase = ''
-              mkdir $out
-            '' +
-            (if pkgs.system == "i686-w64-mingw32" then
-              "cp -vr i686-w64-mingw32/. $out/"
-            else
-              "cp -vr x86_64-w64-mingw32/. $out/");
-          }
-          else null;
-
           surfcpp = pkgs.callPackage ./surfcpp.nix {
-            SDL2 = if pkgs.targetPlatform.isWindows then SDL2 else pkgs.SDL2;
-            libjpeg = if pkgs.targetPlatform.isWindows then pkgs.libjpeg_original else pkgs.libjpeg;
+            SDL2 = if pkgs.targetPlatform.isWindows then SDL2-win32.packages.${pkgs.system}.default else pkgs.SDL2;
+            libjpeg = if pkgs.targetPlatform.isWindows
+                      then (pkgs.libjpeg_original.overrideAttrs (oldAttrs: { meta = {}; }))
+                      else pkgs.libjpeg;
             geomcpp = geomcpp.packages.${pkgs.targetPlatform.system}.default;
             logmich = logmich.packages.${pkgs.targetPlatform.system}.default;
             tinycmmc = tinycmmc.packages.${pkgs.targetPlatform.system}.default;
