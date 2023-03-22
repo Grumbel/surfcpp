57df158052ffe30decbcffcaa8d6a6fd22b15930
Ingo Ruhnke <grumbel@gmail.com>
2023-03-22 16:05:15 +0100

    Update flake.nix to nixos-22.11 and gcc12Stdenv

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Software Surface Library";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
@@ -25,6 +25,7 @@
           default = surfcpp;
 
           surfcpp = pkgs.callPackage ./surfcpp.nix {
+            stdenv = pkgs.gcc12Stdenv;
             SDL2 = if pkgs.targetPlatform.isWindows then SDL2-win32.packages.${pkgs.system}.default else pkgs.SDL2;
             libjpeg = if pkgs.targetPlatform.isWindows
                       then (pkgs.libjpeg_original.overrideAttrs (oldAttrs: { meta = {}; }))
